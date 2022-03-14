22d288493cafc44f9a10391139385b0709378ad2
Ashu-Tripathi <tripathiashu080@gmail.com>
2022-03-14 08:41:59 +0530

    authorisation

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,14 +1,22 @@
 const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
-const tokenCheck = async function(req,res,next){
+const authentication = async function(req,res,next){
     let token = req.headers["x-Auth-token"];
     if(!token)  token = req.headers["x-auth-token"];
     if(!token) return res.send({status:false, Warning :"token must be present"})
 
-    let decodedToken = await jwt.verify(token, "SECRET MESSAGE")
+    let decodedToken = jwt.verify(token, "SECRET MESSAGE")
     if(!decodedToken) return res.send({status : false, warning : "Invalid Token"})
 
     next()
 }
 
-module.exports.tokenCheck = tokenCheck;
+const authorisation = async function(req,res,next){
+    let decodedToken = jwt.verify(req.headers["x-auth-token"],"SECRET MESSAGE");
+    if(decodedToken.userId != req.params.userId) return res.send({Warning: "You are not allowed to make changes in this data"})
+    next()
+}
+
+module.exports.authentication = authentication;
+module.exports.authorisation = authorisation;
--- src/routes/routeAsign.js
+++ src/routes/routeAsign.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller= require("../controllers/controller")
-const middleware = require('../middleware/middleware')
+const auth = require('../middleware/auth')
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -12,10 +12,10 @@ router.post("/users", controller.createUser  )
 router.post("/login", controller.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId",middleware.tokenCheck, controller.getUserData)
+router.get("/users/:userId",auth.authentication, auth.authorisation ,controller.getUserData)
 
-router.put("/users/:userId", middleware.tokenCheck, controller.updateUser)
+router.put("/users/:userId", auth.authentication, auth.authorisation,controller.updateUser)
 
-router.delete('/users/:userId',middleware.tokenCheck, controller.deleteUser)
+router.delete('/users/:userId', auth.authentication,auth.authorisation, controller.deleteUser)
 
 module.exports = router;
